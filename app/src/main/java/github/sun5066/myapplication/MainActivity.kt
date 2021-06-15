92684897178f8aaf47d040ff1a309523b9e71143
sun5066 <alstjr4434@naver.com>
2021-06-15 23:37:22 +0900

    hilt viewModel 적용

--- app/src/main/java/github/sun5066/myapplication/MainActivity.kt
+++ app/src/main/java/github/sun5066/myapplication/MainActivity.kt
@@ -3,17 +3,23 @@ package github.sun5066.myapplication
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import dagger.hilt.android.AndroidEntryPoint
+import github.sun5066.myapplication.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private var dataBinding: ActivityMainBinding? = null
     private val todoViewModel: TodoViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        todoViewModel.init("zzz")
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        dataBinding?.viewModel = todoViewModel
+        dataBinding?.lifecycleOwner = this
+
+        todoViewModel.init("init ViewModel")
     }
 }
--- app/src/main/java/github/sun5066/myapplication/TodoViewModel.kt
+++ app/src/main/java/github/sun5066/myapplication/TodoViewModel.kt
@@ -1,21 +1,22 @@
 package github.sun5066.myapplication
 
+import android.app.Application
+import android.content.Context
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class TodoViewModel @Inject constructor(private val mSavedStateHandle: SavedStateHandle): ViewModel() {
+class TodoViewModel @Inject constructor(
+    application: Application,
+    private val savedStateHandle: SavedStateHandle
+) : AndroidViewModel(application) {
 
     private val _str = MutableLiveData("")
     val str: LiveData<String> get() = _str
 
     fun init(str: String) {
-        Log.d("123", "str: $str")
-        _str.postValue(str)
+        _str.value = str
     }
 }
